2f8c427a22416d83809cd45f5f0865a090570d0e
skittles <summeywummey@gmail.com>
2023-10-26 12:08:53 -0400

    Update misc_playground.py

--- misc_playground.py
+++ misc_playground.py
@@ -1,2 +1,39 @@
-list = [0.34, '6', 'SI106', 'Python', -2]
-print(len(list[1:-3]))
+"""
+    Joining of two tables
+"""
+def join_loop(table1, table2, join_condition):
+    # empty result list
+    res = []
+
+    # table1 loop
+    for row1 in table1:
+        # table2 loop
+        for row2 in table2:
+            # filter callback
+            if join_condition(row1, row2, 0):
+                res.append(row1 + row2)
+    res = order_callback(res)
+    return res
+
+
+"""
+    This is an example for simple join callback, which can be used as a filter.
+"""
+def join_condition_callback(row1, row2, condition=any):
+    return row1[condition] == row2[condition]
+
+
+"""
+    Sorting a resulting table.
+"""
+def order_callback(raw):
+    return sorted(raw)
+
+
+# sample tables
+table1 = [('cat', 'tom'), ('mice', 'jerry'), (3, 'Charlie')]
+table2 = [('cat', 'mice'), ('mice', 'cheese'), (4, 40)]
+
+result = join_loop(table1, table2, join_condition_callback)
+
+print(result)
